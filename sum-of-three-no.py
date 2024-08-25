d2fe6eb8c3ea54b2494ba53ee58aac481c5ff89e
SushyamNagallapati <sushyamraju@gmail.com>
2024-08-25 23:50:24 +0530

    changed the script from 3 to 4 numbers

--- sum-of-three-no.py
+++ sum-of-three-no.py
@@ -1,14 +1,15 @@
-def sum_of_three_numbers(a, b, c):
-    return a + b + c
+def sum_of_three_numbers(a, b, c, d):
+    return a + b + c + d
 
 if __name__ == "__main__":
     # Ask the user to input three numbers
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     num3 = float(input("Enter the third number: "))
+    num4 = float(input("Enter fourth number: "))
 
     # Calculate the sum
-    result = sum_of_three_numbers(num1, num2, num3)
+    result = sum_of_three_numbers(num1, num2, num3, num4)
 
     # Output the result
-    print(f"The sum of {num1}, {num2}, and {num3} is: {result}")
+    print(f"The sum of {num1}, {num2}, {num3}, and {num4} is: {result}")
