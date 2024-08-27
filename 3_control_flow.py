8faeba14ebf6968f70e6a903016f90298fd73ace
SushyamNagallapati <sushyamraju@gmail.com>
2024-08-27 22:12:13 +0530

    Ternary Operator

--- 3_control_flow.py
+++ 3_control_flow.py
@@ -14,4 +14,26 @@ elif temperature < 10:  #elif is the shortcut for "else if"
     print("It's cold")
 else: 
     print("It's Normal")
-  
+
+
+#Ternary Operator
+#type1
+age = 14
+if age>= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+#type2
+age = 50
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Elgible"
+print(message)
+    
+#type3
+age = 23
+message = "Eligible" if age >= 18 else "Not Eligible"
+print(message)
+    
