7a1a7813ee899caaad77ead3e68fae2a1d465f84
SushyamNagallapati <sushyamraju@gmail.com>
2024-08-27 21:46:53 +0530

    Conditional Statements

--- 3_control_flow.py
+++ 3_control_flow.py
@@ -5,3 +5,13 @@
  # <= Less than equal to
  # == Equal to
  # != Not equal to
+ 
+#Conditional Statements
+temperature = 8.9
+if temperature > 30: #if statement should be terinated by colon(:)
+    print("It's very Hot")
+elif temperature < 10:  #elif is the shortcut for "else if"
+    print("It's cold")
+else: 
+    print("It's Normal")
+  
