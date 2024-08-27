18715146f65c6ad984c2ca44cb30626f45497b72
SushyamNagallapati <sushyamraju@gmail.com>
2024-08-27 11:13:39 +0530

    Concept of Strings

--- 2_primitive_types.py
+++ 2_primitive_types.py
@@ -4,4 +4,10 @@ rating = 4.99 #float
 is_published =  True #boolean values sould start with capital letter
 course_name = "Python Programing" #String
 
-
+name = "Sushyam Nagallapati"
+print(len(name))
+print(name[0])
+print(name[0:4])
+print(name[0:])
+print(name[:])
+print(name[-5])
