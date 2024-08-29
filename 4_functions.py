28a2b0ee0a8f60e935bc8ef2ff6fb1f52cc1bbf4
SushyamNagallapati <sushyamraju@gmail.com>
2024-08-29 16:01:43 +0530

    Scope [Local & Global Variable]

--- 4_functions.py
+++ 4_functions.py
@@ -47,10 +47,24 @@ def multiply(*numbers):
 print(multiply(2, 3, 4, 5))
 
 
-#**args
+#**args ---> When we use ** we can pass multiplekey value pairs or multiple keyword arguments to a function, and python will automatically package them into a dictionary.
 def save_user(**user):
     print(user["age"])
     
 save_user(id=1, name="Sushyam", age=23)
 
 
+#SCOPE
+#There are two variables in scope (1)Local variable (2)Global variable
+
+#Local variable -----> will be used inside the defined object
+def greet(name):
+    message = "a"
+greet("sai")
+print(message)
+
+#Global variable ----> will be used outside the defined object
+message = "a"
+def greet(name):
+    greet("sai")
+print(message)
