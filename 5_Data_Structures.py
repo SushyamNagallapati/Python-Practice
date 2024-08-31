e90652dad683c67cef04fb986647091a4970ffbc
SushyamNagallapati <sushyamraju@gmail.com>
2024-08-31 12:28:53 +0530

    Sorting Lists

---
+++ 5_Data_Structures.py
@@ -0,0 +1,81 @@
+#Lists ---------> We can store string, numbers, booleans and even lists inside the sq brackets "[]"
+# letters = ["a", "b", "c", "d"]      #Lists are used to store multiple items in a single variable
+# print(letters)
+
+# matrix = [[0, 1], [2, 5], [4, 8]]
+# print(matrix)
+
+# zeros = [0] * 10
+# combined = zeros + letters
+# print(combined)
+
+# numbers = list(range(20))
+# print(numbers)
+
+# chars = list("Sushyam Nagallapati")
+# print(chars)
+# print(len(chars))
+
+
+#Accessing Items ----> We can use square brackets to access individual items in the list
+# type 1
+# letters = ["a", "b", "c", "d"]
+# letters[0] = "A"
+# print(letters[:])
+# print(letters[::2])
+
+# #type 2
+# numbers = list(range(20))
+# print(numbers)
+# print(numbers[::2])
+
+#List Unpacking
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# first, second, *others, last  = numbers
+# print(first, last)
+# print(*others)
+
+
+#Looping over lists
+#type 1
+# letters = ["a", "b", "c"]
+# for letter in enumerate(letters):      
+#     print(letter[0], letter[1])
+
+#type 2
+# letters = ["a", "b", "c"]
+# items = (0, "a")
+# index, letter = items
+# for index, letter in enumerate(letters):
+#     print(index, letter)
+    
+
+#Adding/Removing Items
+# letters = ["a", "b", "c"]
+#to Add
+# letters.append("d")  #---> .append - to add at the end of the list
+# letters.insert(0, "-")  #---> .insert - to add at the beginning of the list
+# print(letters)
+
+#to Remove
+# letters.pop(0)
+# letters.remove("b")
+# del letters[0:1]
+# letters.clear()
+# print(letters)
+
+#Finding Items
+# letters = ["a", "b", "c", "d"]
+# print(letters.count("d"))
+# if "d" in letters:
+#     print(letters.index("d"))
+
+
+#Sorting Lists
+numbers = [1, 2, 5, 78, 95, 45, 3, 4, 50]
+numbers.sort()
+print(numbers)
+
+
+
--- Practice.py
+++ Practice.py
@@ -4,3 +4,85 @@ for number in range(1, 10):
         count += 1
         print(number)
 print(f"We have {count} even numbers")
+
+
+
+
+
+
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "Fizzbuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+
+print(fizz_buzz(15)) 
+
+
+
+
+
+
+num = int(input("Enter a number: "))
+message = "Even" if num % 2 == 0 else "Odd"
+print(message)
+
+
+
+
+
+for num in range(1, 11):
+    print(num)
+
+
+
+
+num = int(input("Enter a number: "))
+for divisor in range(2, num):
+    if num % divisor == 0:
+        print(divisor)
+
+
+
+
+num = int(input("Enter a number: "))
+
+is_prime = True
+
+for i in range(2, num):
+    if num % i == 0:
+        is_prime = False
+        break
+
+
+if is_prime:
+    print("%d It is a prime number"%num)
+else:
+    print("%d It is not a prime number"%num)
+
+
+    
+
+num = int(input("Enter a number: "))
+is_prime = True
+
+for i in range(2, num):
+    if num % i == 0:
+        is_prime = False
+        break
+
+if is_prime:
+    print("%d Is a prime number"%num)
+else:
+    print("%d Is not a prime number"%num)
+
+
+
+
+
+# for num in range(1, 11):
+#     print(num)
