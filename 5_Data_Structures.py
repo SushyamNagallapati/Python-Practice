4bf2f3e0d3de0b176649e2c8eed59c7c5f36030f
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-03 18:06:19 +0530

    Dictionaries

--- 5_Data_Structures.py
+++ 5_Data_Structures.py
@@ -270,3 +270,19 @@ if 1 in first:
     print("Yes")
 else:
     print("No")
+
+
+#Dictionaries ---> Refer notebook
+# point = {"x": 1, "y": 2} 
+point = dict(x=1, y=2)
+point["x"] = 10
+point["z"] = 20
+print(point)
+if "a" in point:
+    print(point["a"])
+print(point.get("a", 0))
+del point["x"]
+print(point)
+for key, value in point.items():
+    print(key, value)
+
