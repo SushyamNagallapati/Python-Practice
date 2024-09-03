1cafe909639328a56c720def5f7bd666f4e6c8c6
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-03 12:07:29 +0530

    Stacks

--- 5_Data_Structures.py
+++ 5_Data_Structures.py
@@ -173,3 +173,22 @@ list2 = [10, 20, 30]
 print(list(zip(list1, list2)))
 
 print(list(zip("abc", list1, list2))) #--->  We can also pass a string 
+
+
+
+#Stacks
+#Uses the principle LIFO - Last In First Out(The last item added, is the first one to be removed)
+browsing_session = []
+browsing_session.append(1) #----> append method is used to add an item on top of the stack
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+
+last = browsing_session.pop() #----> pop method is used to remove an item on top of the stack
+print(last)
+print(browsing_session)
+print("redirect", browsing_session[-1]) #---> index -1 "[-1]" is used to get the item on top of the stack, before doing that we need to check if our stack is empty or not
+if not browsing_session:
+    print("disable")
+else:
+    print("Good to go")
