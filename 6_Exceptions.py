e4143188b80951a95eae6d50b1db3d660653237e
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-05 13:56:50 +0530

    Raising Exceptions

--- 6_Exceptions.py
+++ 6_Exceptions.py
@@ -55,8 +55,12 @@ else:
 def calculate_xfactor(age):
     if age <= 0:
         raise ValueError("Age cannot be 0 or less") #---> This function raises or throws an exception, if we give it an invalid argument
-    return 10 / 0
+    return 10 / age
 
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
 
 
 
