c21c8432b39d8dc252d085cc37ff6d2473ffee32
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-05 12:46:16 +0530

    Cleaning Up

--- 6_Exceptions.py
+++ 6_Exceptions.py
@@ -18,11 +18,26 @@ print("Execution continues")
 #Handling Different Exceptions
 try:
     age = int(input("Age:"))                        
-    xfactor = 10/age
+    xfactor = 10 / age
 except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age")
 else:
     print("You entered a valid age")
+
+
+#Cleaning Up
+try:
+    file = open("6_Exceptions.py") #---> This helps to open a new terminal
+    age = int(input("Age: "))
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age")
+else:
+    print("No exceptions were thrown")
+finally:                     
+    file.close() #---> Finally is used because it mentions, at the end close the file after all the process
+
+
 
 
 
