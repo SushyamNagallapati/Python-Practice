1c70631924e0a3d67be7e924e2763016d818c637
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-05 12:05:06 +0530

    Handling Different Exceptions

--- 6_Exceptions.py
+++ 6_Exceptions.py
@@ -9,8 +9,20 @@ try:    #The try block contains code that might raise an exception
 except ValueError as ex:
     print("You didn't enter a valid age")
     print(ex) #prints the exception message associated with the ValueError, which provides ore detail about what went wrong
-    print(type(ex)) #prints tha type of the exception object
+    print(type(ex)) #prints the type of the exception object
 else:
     print("You entered a valid age")
 print("Execution continues")
+
+
+#Handling Different Exceptions
+try:
+    age = int(input("Age:"))                        
+    xfactor = 10/age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age")
+else:
+    print("You entered a valid age")
+
+
 
