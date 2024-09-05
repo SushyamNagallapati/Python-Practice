2a76cf086014bb84cfe393938bee619daee7de46
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-05 14:18:56 +0530

    Cost of Raising Exceptions

--- 6_Exceptions.py
+++ 6_Exceptions.py
@@ -63,6 +63,33 @@ except ValueError as error:
     print(error)
 
 
+#Cost of Raising Exceptions #---> Refer notebook for explanation
+from timeit import timeit
+code1 ="""
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less") 
+    return 10 / age
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    pass
+    """
+
+code2 ="""
+def calculate_xfactor(age):
+    if age <= 0:
+        return None 
+    return 10 / age
+
+
+xfactor = calculate_xfactor(-1)
+if xfactor == None:
+    pass
+    """
+print("First code=", timeit(code1, number=10000))
+print("Second code=", timeit(code2, number=10000))
 
 
 
