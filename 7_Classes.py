fb9c160cb043305249240eb3c065ff1bae6ecc50
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-05 16:59:01 +0530

    Creating Classes

--- 7_Classes.py
+++ 7_Classes.py
@@ -5,3 +5,13 @@
 #Example
 #Class - Human
 #Object - Sai, Nirmala,....
+
+
+#Creating Classes
+class Point:       #While creating a class, the first letter of every word should be in upper case
+    def draw(self):
+        print("draw")
+
+point = Point()
+print(type(point))
+print(isinstance(point, Point)) #isinstance - If we have an object, and we want to know if this object is an instance of a given class, we can use isinstance function
