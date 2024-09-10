15a4eefbbaa3329f50a47013b3fa1cb9828521c0
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-10 10:06:01 +0530

    Class vs Instance Methods

--- 7_Classes.py
+++ 7_Classes.py
@@ -50,3 +50,19 @@ point.draw()
 
 another = Point(3, 4)
 another.draw()
+
+#Class vs Instance Methods
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
+    
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+point = Point.zero()
+point.draw()
