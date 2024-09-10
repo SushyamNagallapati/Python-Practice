fd46b37b629e4b7784b1c1fa874e1df895f5e6f5
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-10 11:31:38 +0530

    Supporting Arithematic Operators

--- 7_Classes.py
+++ 7_Classes.py
@@ -99,3 +99,18 @@ point = Point(10, 20)
 other = Point(1, 2)
 # print(point == other)
 print(point > other)
+
+
+#Supporting Arithmetic Operators - refer note for link
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    
+point = Point(10, 20)
+other = Point(1, 2)
+combined = point + other
+print(combined.x, combined.y)
