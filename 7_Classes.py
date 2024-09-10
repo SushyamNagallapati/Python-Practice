a1ee118d330a6dc5a85bdf4ee8805e177315b60d
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-10 10:59:50 +0530

    Magic Methods

--- 7_Classes.py
+++ 7_Classes.py
@@ -66,3 +66,18 @@ class Point:
 
 point = Point.zero()
 point.draw()
+
+#Magic Methods
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+point = Point(1, 2)
+print(point) #or print(str(point)) ---> gives the same result 
