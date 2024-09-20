cb4f24b2468ca41e59d765a30f1e0e03842928e2
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-20 12:46:13 +0530

    What are APIs

---
+++ 11_popular_python_packages.py
@@ -0,0 +1,16 @@
+#Refer notes
+
+#Explained about;
+#1. What are API's
+    
+#   API - Application Programming Interface
+#   These APIs are end points that are publicly accessible on the internet
+#   We can access by URLs
+#   Every http request has a method. That method determines what we are going to use this request for.
+#   There are few request methods they are;
+#        -> GET - for geting data
+#        -> POST - for creating data
+#        -> PUT - for updating data
+#        -> DELETE - for deleting data
+
+
