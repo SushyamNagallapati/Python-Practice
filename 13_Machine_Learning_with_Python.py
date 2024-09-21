336b50f602dc30b548028ffbeb7e004d3bcb42a4
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-22 00:53:52 +0530

    1. What is ML

---
+++ 13_Machine_Learning_with_Python.py
@@ -0,0 +1,11 @@
+#1.What is Machine Learning
+
+#-> ML is a subset of AI
+#-> ML Applications are as follows
+#       1. Self-driving Cars
+#       2. Robotics
+#       3. Language Processing
+#       4. Vision Processing
+#       5. Forecasting Stock Market Trends
+#       6. Weather
+#       7. Games etc...
