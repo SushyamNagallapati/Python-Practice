d0e3b0d368b94005d6305a5a0841b4bc888663de
SushyamNagallapati <sushyamraju@gmail.com>
2024-09-21 21:02:49 +0530

    .

--- 11_popular_python_packages.py
+++ 11_popular_python_packages.py
@@ -1,8 +1,8 @@
-#Refer notes and Video
+# Refer notes and Video
 
-#Explained about;
-#2. What are API's
-    
+# Explained about;
+# 2. What are API's
+
 #   API - Application Programming Interface
 #   These APIs are end points that are publicly accessible on the internet
 #   We can access by URLs
@@ -14,98 +14,86 @@
 #        -> DELETE - for deleting data
 
 
-#3. Yelp API
+# 3. Yelp API
 
-#-> Intro to Yelp
-#-> How to create an Yelp account
-
-
-#4. Searching for Businesses
-
-#-> How to search businesses using Yelp API link
-#-> VSCode explanation
-#-> How to search the business by filter process
+# -> Intro to Yelp
+# -> How to create an Yelp account
 
 
+# 4. Searching for Businesses
 
-#5. Hiding API Keys
-
-#-> How to hide the API key by creating a config.py file and .gitignore
-#-> We should not share the API key in public
-
-
-
-#6. Sending Text Messages
-
-#-> How create Twilio account
-#-> With Twilio we can get a free access phone number, with this no. we can send messages, call, ect...
-#-> In VSCode we have to hide the API
+# -> How to search businesses using Yelp API link
+# -> VSCode explanation
+# -> How to search the business by filter process
 
 
+# 5. Hiding API Keys
 
-#7. Web Scrapping
+# -> How to hide the API key by creating a config.py file and .gitignore
+# -> We should not share the API key in public
 
-#-> Not every website has an API for us to work with.
-#-> In that situation, the only way to get the data we want is to parse the html behind the webpage, get rid of all thhe html tags
+
+# 6. Sending Text Messages
+
+# -> How create Twilio account
+# -> With Twilio we can get a free access phone number, with this no. we can send messages, call, ect...
+# -> In VSCode we have to hide the API
+
+
+# 7. Web Scrapping
+
+# -> Not every website has an API for us to work with.
+# -> In that situation, the only way to get the data we want is to parse the html behind the webpage, get rid of all thhe html tags
 #   and extract the actual data. This technique is called Web Scrapping
-#-> In the video, we will learn how to scrap the questions from StackOverflow, in detail.
+# -> In the video, we will learn how to scrap the questions from StackOverflow, in detail.
 
 
+# 8. Browser Automation
 
-#8. Browser Automation
-
-#-> Selenium - With this we can automate our browser, so we can write a bunch of scripts to test various functions on a website.
-#-> In video, Mosh taught how to sign in git hub by using VSCode
-#-> Refer link "https://selenium-python.readthedocs.io" - in this link read about the Waits and Page Objects
-
+# -> Selenium - With this we can automate our browser, so we can write a bunch of scripts to test various functions on a website.
+# -> In video, Mosh taught how to sign in git hub by using VSCode
+# -> Refer link "https://selenium-python.readthedocs.io" - in this link read about the Waits and Page Objects
 
 
+# Working with PDFs
 
-#Working with PDFs
-
-#-> How to open the pdf file
-#-> How to print the number of pages
-#-> How to rotate the page
-#-> How to add a page
-#-> How to merge/combine two PDF files
-
+# -> How to open the pdf file
+# -> How to print the number of pages
+# -> How to rotate the page
+# -> How to add a page
+# -> How to merge/combine two PDF files
 
 
-#Working with Excel Spreadsheets
+# Working with Excel Spreadsheets
 
-#-> How to get the no. of sheets
-#-> How to create a worksheet
-#-> How to remove an existing worksheet
-#-> How to access individual cell (or) range of cells
-#-> How to change the value in the cell
-#-> How to add a row at the end of the sheet
-#-> How to insert a row and column at a given index
-#-> How to delete rows and columns
-#-> How to save in a new file
-
-
+# -> How to get the no. of sheets
+# -> How to create a worksheet
+# -> How to remove an existing worksheet
+# -> How to access individual cell (or) range of cells
+# -> How to change the value in the cell
+# -> How to add a row at the end of the sheet
+# -> How to insert a row and column at a given index
+# -> How to delete rows and columns
+# -> How to save in a new file
 
 
-#Command Query Separation Principle
+# Command Query Separation Principle
 
-#-> This principle states that our methods (or) functions should either be commands that perform an action to change the state of a system, (or)
+# -> This principle states that our methods (or) functions should either be commands that perform an action to change the state of a system, (or)
 #   queries that return an answer to the caller without changing the state (or) causing side effects
 
-#-> So our methods should either be commands (or) queries, but not both
-#-> Explains the principal using the same spreadsheet as example
+# -> So our methods should either be commands (or) queries, but not both
+# -> Explains the principal using the same spreadsheet as example
 
 
+# NumPy - Refer PyNumbers Repository in Git
 
+# -> How to create Multi-dimensional array
 
-#NumPy - Refer PyNumbers Repository in Git
+# -> Methods for creating arrays and initializing
 
-#-> How to create Multi-dimensional array
+# -> Functions for performing computations on arrays
 
-#-> Methods for creating arrays and initializing 
+# -> How to perform Arithematic operations with arrays and numbers
 
-#-> Functions for performing computations on arrays
-
-#-> How to perform Arithematic operations with arrays and numbers
-
-#-> Real world example, on how to convert an array of dimension from inch to centimeter
-
+# -> Real world example, on how to convert an array of dimension from inch to centimeter
