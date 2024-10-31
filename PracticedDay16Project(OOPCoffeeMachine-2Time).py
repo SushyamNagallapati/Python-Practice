2023d3d3494ca1772ef1233e8d73aa2f78a79d33
Sushyam Nagallapati <sushyamraju@gmail.com>
2024-10-31 10:25:10 +0530

    Practiced Day 16 Project (OOP Coffee Machine - 2 Time)

---
+++ PracticedDay16Project(OOPCoffeeMachine-2Time).py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    items = menu.get_items()
+    user_choice = input(f"What would you like? ({items}): ").lower()
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
