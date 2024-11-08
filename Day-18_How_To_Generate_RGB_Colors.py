14b8262fd3cd596061c9ab1424a422a980a58b6e
Sushyam Nagallapati <sushyamraju@gmail.com>
2024-11-08 13:06:56 +0530

    Day-18 How To Generate RGB Colors

---
+++ Day-18_How_To_Generate_RGB_Colors.py
@@ -0,0 +1,35 @@
+import turtle as t
+from turtle import Screen
+import random
+
+sai = t.Turtle()
+
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+direction = [0, 90, 180, 270]
+
+sai.pensize(10)
+sai.speed("fastest")
+sai.shape("turtle")
+
+for _ in range(200):
+    sai.color(random_color())
+    sai.forward(30)
+    sai.setheading(random.choice(direction))
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
