4e7dd23e288273ed1267117f2adfc0cdadcb7875
SushyamNagallapati <sushyamraju@gmail.com>
2024-11-09 19:33:56 +0530

    Day 18 Topic 3 - Importing Modules, Installing Packages, and Working with Aliases

--- HelloWorld/ecommerce/app.py
+++ HelloWorld/ecommerce/app.py
@@ -532,6 +532,7 @@
 #Topics
 #1. Understanding Turtle Graphics and How to use the Documentation
 #2. Turtle Challenge 1 - Draw a Square
+#3. Importing Modules, Installing Packages, and Working with Aliases
 
 
 
