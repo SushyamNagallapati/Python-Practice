812ee406b52ba86125b2df409b0298a8c60e346b
SushyamNagallapati <sushyamraju@gmail.com>
2024-11-09 19:38:04 +0530

    Day 18 Challenge 3 - Drawing Different Shapes

--- HelloWorld/ecommerce/app.py
+++ HelloWorld/ecommerce/app.py
@@ -534,6 +534,7 @@
 #2. Turtle Challenge 1 - Draw a Square
 #3. Importing Modules, Installing Packages, and Working with Aliases
 #4. Turtle Challenge 2 - Draw a Dashed Line
+#5. Turtle Challenge 3 - Drawing Different Shapes
 
 
 
