d3c63012fb6f0f76396c7720da4fa563d04fe159
SushyamNagallapati <sushyamraju@gmail.com>
2024-12-06 23:35:44 +0530

    Day 30 Topic - 5 Code Exercise: Exception Handling in the NATO Phonetic Alphabet Project

--- HelloWorld/ecommerce/app.py
+++ HelloWorld/ecommerce/app.py
@@ -694,3 +694,4 @@
 #2. Raising your own Exceptions
 #3. Coding Exercise - IndexError Handling
 #4. Coding Exercise - KeyError Handling
+#5. Code Exercise: Exception Handling in the NATO Phonetic Alphabet Project
